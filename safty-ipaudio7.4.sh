3161544cf5f44adf329cad8226df45933258cad0
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2024-05-07 20:02:51 +0300

    Add files via upload

--- safty-ipaudio7.4.sh
+++ safty-ipaudio7.4.sh
@@ -1,15 +1,21 @@
-
 #!/bin/sh
 #
 
-wget -O /tmp/ipaudio7.6.tar.gz"https://raw.githubusercontent.com/tarekzoka/ipaudio/main/safty-ipaudio7.4.tar.gz"
+wget -O /tmp/safty-ipaudio7.4.tar.gz "https://raw.githubusercontent.com/tarekzoka/ipaudio/main/safty-ipaudio7.4.tar.gz"
 
 tar -xzf /tmp/*.tar.gz -C /
 
-rm -r /tmp/safty-ipaudio7.4
+rm -r /tmp/safty-ipaudio7.4.tar.gz
+
+wait
+
+echo tarek hanfy
 
 killall -9 enigma2
 
 sleep 2;
 
+wait
+
+exit 0  
 
