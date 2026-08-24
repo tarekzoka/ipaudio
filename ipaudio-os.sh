e854b6618c7ba9e17d0659c8ea009e9139d8ac90
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2026-08-24 21:53:04 +0300

    Fix URL formatting in ipaudio-os.sh

--- ipaudio-os.sh
+++ ipaudio-os.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
 
-wget -O /tmp/ipaudio-os.tar.gz "https://raw.githubusercontent.com/tarekzoka/ipaudio/raw/main/ip audio-os.tar.gz"
+wget -O /tmp/ipaudio-os.tar.gz "https://raw.githubusercontent.com/tarekzoka/ipaudio/raw/main/ipaudio-os.tar.gz"
 
 tar -xzf /tmp/*.tar.gz -C /
 
